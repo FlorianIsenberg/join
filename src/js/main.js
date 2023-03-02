1846c3bda5e367bda6ddd7d7ad1c37fbf36e1438
Michel Bente <bentemichel@web.de>
2023-03-02 09:55:19 +0100

    update miniback

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+    setURL('https://gruppe-07i.developerakademie.net/smallest_backend_ever');
+    let users = [];
+
+    async function init() {
+        await downloadFromServer(); 
+        users = JSON.parse(backend.getItem('users')) || [];
+
+        }
+    
