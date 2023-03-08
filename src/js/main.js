ab25f19c5686dfa345d6bbfa3e5da3675e891ed6
FlorianIsenberg <florianisenberg@gmail.com>
2023-03-08 09:17:25 +0100

    trash

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
+setURL("https://gruppe-07i.developerakademie.net/smallest_backend_ever");
+let users = [];
 
-    setURL('https://gruppe-07i.developerakademie.net/smallest_backend_ever');
-    let users = [];
-
-    async function init() {
-        await downloadFromServer(); 
-        users = JSON.parse(backend.getItem('users')) || [];
-        backend.setItem('Test', 'Hallo');
-        }
-    
+async function init() {
+  await downloadFromServer();
+  users = JSON.parse(backend.getItem("users")) || [];
+  backend.setItem("Test", "Hallo");
+}
