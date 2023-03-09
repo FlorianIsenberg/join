2d95dfbfa1996be3d24e32a3f2c2cce1d24f7e2c
Michel Bente <bentemichel@web.de>
2023-03-09 11:39:16 +0100

    added some changes in drag and drop area

--- src/js/board.js
+++ src/js/board.js
@@ -1,32 +1,100 @@
 let todos = [{
-    'id': 0,
-    'category': 'Design',
-    'titel': 'Website redesign',
-    'note': 'Modify the contents of the main website..',
-    'progresslevel': '1/2 Done',
-    'progressbar': '<img src="progressbar.svg">',
-    'users': 'pic'
-}];
+    id: 0,
+    title: 'Website redesign',
+    description: 'Modify the contents of the main website..',
+    date: '',
+    priority: 'low',
+    assignedTo: ['Sven Müller'],
+    department: 'design',
+    taskStatus: 'todo',
+    subtasks: [],
+    subtasksDone: [0.5],
+    category: 'todo'   
+},
+{
+    id: 1,
+    title: 'Call potential clients',
+    description: 'Make the product presentation to prospective buyers',
+    date: '',
+    priority: 'high',
+    assignedTo: ['Alex Sam'],
+    department: 'sales',
+    taskStatus: 'inprogress',
+    subtasks: [],
+    subtasksDone: [],
+    category: 'inprogress',  
+},
+{
+    id: 2,
+    title: 'Accounting invoices',
+    description: 'Write open invoices for customer',
+    date: '',
+    priority: 'medium',
+    assignedTo: ['Mark Becker'],
+    department: 'backoffice',
+    taskStatus: 'awaitingfeedback',
+    subtasks: [],
+    subtasksDone: [],
+    category: 'awaitingfeedback',  
+},
+{
+    id: 3,
+    title: 'Video cut',
+    description: 'Edit the new company video',
+    date: '',
+    priority: 'medium',
+    assignedTo: ['Hans Kaiser'],
+    department: 'media',
+    taskStatus: 'awaitingfeedback',
+    subtasks: [],
+    subtasksDone: [],
+    category: 'awaitingfeedback',  
+},
+{
+    id: 4,
+    title: 'Social media strategy',
+    description: 'Develop an ad campaign for brand positioning',
+    date: '',
+    priority: 'low',
+    assignedTo: ['Bernd Ziegler'],
+    department: 'marketing',
+    taskStatus: 'done',
+    subtasks: [],
+    subtasksDone: [],
+    category: 'done',  
+}
+];
 
 let currentDraggedElement;
 
 function updateHTML() {
-    let open = todos.filter (t => t['category'] =='open');
+    let todo = todos.filter(t => t['category'] == 'todo');
+    let inprogress = todos.filter(t => t['category'] == 'inprogress');
+    let awaitingfeedback = todos.filter(t => t['category'] == 'awaitingfeedback');
+    let done = todos.filter(t => t['category'] == 'done');
 
-    document.getElementById('open').innerHTML = '';
-
-    for (let index = 0; index < open.length; index++) {
-        const element = open[index];
-        document.getElementById('open').innerHTML += generateTodoHTML(element);
+    document.getElementById('todo').innerHTML = '';
+    for (let index = 0; index < todo.length; index++) {
+        const element = todo[index];
+        document.getElementById('todo').innerHTML += generateTodoHTML(element);
     }
 
-let closed = todos.filter(t => t['category'] == 'closed');
+    document.getElementById('inprogress').innerHTML = '';
+    for (let index = 0; index < inprogress.length; index++) {
+        const element = inprogress[index];
+        document.getElementById('inprogress').innerHTML += generateTodoHTML(element);
+    }
 
-document.getElementById('closed').innerHTML = '';
+    document.getElementById('awaitingfeedback').innerHTML = '';
+    for (let index = 0; index < awaitingfeedback.length; index++) {
+        const element = awaitingfeedback[index];
+        document.getElementById('awaitingfeedback').innerHTML += generateTodoHTML(element);
+    }
 
-    for (let index = 0; index < closed.length; index++) {
-        const element = closed[index];
-        document.getElementById('closed').innerHTML += generateTodoHTML(element);
+    document.getElementById('done').innerHTML = '';
+    for (let index = 0; index < done.length; index++) {
+        const element = done[index];
+        document.getElementById('done').innerHTML += generateTodoHTML(element);
     }
 }
 
@@ -34,8 +102,27 @@ function startDragging(id) {
     currentDraggedElement = id;
 }
 
-
 function generateTodoHTML(element) {
-    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element.todos}</div>`;
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(category) {
+    let task = todos[currentDraggedElement];
+    task.category = category;
+    todos = todos.filter(t => t.id != task.id).concat(task);
+    updateHTML();
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add('drag-area-highlight');
+}
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('drag-area-highlight');
+}
+
+
 
