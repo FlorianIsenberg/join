f78d0394a3759f25118b0d803697a304512cf83d
FlorianIsenberg <florianisenberg@gmail.com>
2023-03-14 08:51:39 +0100

    update resetJs

--- src/js/resetPassword.js
+++ src/js/resetPassword.js
@@ -1,6 +1,31 @@
-function onPageLoad() {
-  email = getEmailUrLParameter();
-  users = getUsers();
+setURL("https://gruppe-07i.developerakademie.net/smallest_backend_ever");
+
+async function init() {
+  await downloadFromServer();
+  users = JSON.parse(backend.getItem("users")) || [];
+}
+
+async function resetPassword() {
+  let email = getEmailUrLParameter();
+  let newPassword = document.getElementById("newPassword").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+
+  let user = users.find((u) => u.email === email);
+  if (user.email === email) {
+    if (newPassword === confirmPassword) {
+      user.password = newPassword.value;
+      await backend.setItem("users", JSON.stringify(users));
+      console.log("Password reset successful!");
+      //BESTÄTIGUNGSANIMATION
+      setTimeout(function () {
+        window.location.href = "../../index.html";
+      }, 3000);
+    } else {
+      console.log("Passwords do not match");
+      newPassword.value = "";
+      confirmPassword.value = "";
+    }
+  }
 }
 
 function getEmailUrLParameter() {
@@ -8,12 +33,4 @@ function getEmailUrLParameter() {
   const urlParams = new URLSearchParams(queryString);
   const email = urlParams.get("email");
   return email;
-}
-
-function getUsers() {
-  return JSON.parse(localStorage.getItem("users")) || [];
-}
-
-function onSubmit(event) {
-  event.preventDefault();
 }
